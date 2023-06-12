4969946c82492fcd96013d72cddb66dbb0416385
Neareyn <yhazukashi@gmail.com>
2023-06-12 11:50:17 +0200

    changed timer + fixed it

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ let randomNumberArray = [];
 let amount = "owo"
 let RightNumber = "owo"
 let phase = 1;
-let timerAmount = 510;
+let maxtimerAmount = 800;
+let timerAmount = 800;
 let isAllowed = true;
 let isTimerAllowed = true;
+let guessTime = 0;
+let DeleteThis = 0;
+let o = 0;
 
 function generateNumbers(amount){
     for (let i = 1; i <= amount; i++) {
@@ -19,30 +23,30 @@ function generateNumbers(amount){
         if(i === 0){
             document.getElementById("SkillName" + i).innerText = "A: " + skillData[variableValue].name.en
             if(i === RightNumber){
-                document.getElementById("SkillIcon").style.backgroundImage = "url(/FlyffFrenzy/img/colored/" + skillData[variableValue].icon + ")"
-                document.getElementById("ID").innerText = "ID: " + variableValue
+                document.getElementById("SkillIcon").style.backgroundImage = "url(./img/colored/" + skillData[variableValue].icon + ")"
+                //document.getElementById("ID").innerText = "ID: " + variableValue
 
             }
         }
         if(i === 1){
             document.getElementById("SkillName" + i).innerText = "B: " + skillData[variableValue].name.en
             if(i === RightNumber){
-                document.getElementById("SkillIcon").style.backgroundImage = "url(/FlyffFrenzy/img/colored/" + skillData[variableValue].icon + ")"
-                document.getElementById("ID").innerText = "ID: " + variableValue
+                document.getElementById("SkillIcon").style.backgroundImage = "url(./img/colored/" + skillData[variableValue].icon + ")"
+                //document.getElementById("ID").innerText = "ID: " + variableValue
             }
         }
         if(i === 2){
             document.getElementById("SkillName" + i).innerText = "C: " + skillData[variableValue].name.en
             if(i === RightNumber){
-                document.getElementById("SkillIcon").style.backgroundImage = "url(/FlyffFrenzy/img/colored/" + skillData[variableValue].icon + ")"
-                document.getElementById("ID").innerText = "ID: " + variableValue
+                document.getElementById("SkillIcon").style.backgroundImage = "url(./img/colored/" + skillData[variableValue].icon + ")"
+                //document.getElementById("ID").innerText = "ID: " + variableValue
             }
         }
         if(i === 3){
             document.getElementById("SkillName" + i).innerText = "D: " + skillData[variableValue].name.en
             if(i === RightNumber){
-                document.getElementById("SkillIcon").style.backgroundImage = "url(/FlyffFrenzy/img/colored/" + skillData[variableValue].icon + ")"
-                document.getElementById("ID").innerText = "ID: " + variableValue
+                document.getElementById("SkillIcon").style.backgroundImage = "url(./img/colored/" + skillData[variableValue].icon + ")"
+                //document.getElementById("ID").innerText = "ID: " + variableValue
             }
         }
         console.log(variableName + ": " + variableValue);
@@ -109,45 +113,39 @@ function delay(milliseconds){
 
 async function restartGame(){
     console.log("Game Done");
-    await delay(1000);
+    await delay(700);
     console.log("Restarting in:");
-    await delay(1000);
+    await delay(700);
     console.log("3...");
-    await delay(1000);
+    await delay(700);
     console.log("2...");
-    await delay(1000);
+    await delay(700);
     console.log("1...");
-    await delay(1000);
+    await delay(700);
     document.getElementById("SkillName" + 0).style.backgroundColor = "";
     document.getElementById("SkillName" + 1).style.backgroundColor = "";
     document.getElementById("SkillName" + 2).style.backgroundColor = "";
     document.getElementById("SkillName" + 3).style.backgroundColor = "";
     
-    timerAmount = 510;
+    timerAmount = 800;
     isTimerAllowed = true;
     isAllowed = true;
     getTriviaSkill()
 }
 
 async function startTimer(){
-
-    console.log("owo")
-
-    
     for(let i = 0; i < 256; i++){
         if(!isTimerAllowed){
             break; 
         }
         document.getElementById("Timer").style.backgroundColor = "rgb(" + i + ", " + 255 + ", 0)"
         timerAmount--
-        document.getElementById("Timer").style.width = 100/510*timerAmount + "%";
-
+        document.getElementById("Timer").style.width = 100/maxtimerAmount*timerAmount + "%";
+        console.log(timerAmount)
         if(!isTimerAllowed){
             break; 
         }
-        await delay(2);
-        
-
+        await delay(guessTime);
     } 
     if(i = 255)
         for(let o = 255; o > -1; o--){
@@ -156,12 +154,25 @@ async function startTimer(){
             }
             document.getElementById("Timer").style.backgroundColor = "rgb(" + 255 + ", " + o + ", 0)"
             timerAmount--
-            document.getElementById("Timer").style.width = 100/510*timerAmount + "%";
-
+            document.getElementById("Timer").style.width = 100/maxtimerAmount*timerAmount + "%";
+            console.log(o)
             if(!isTimerAllowed){
                 break; 
             }
-            if(o < 1){
+            await delay(guessTime);           
+    }   
+    if(o <= 0){
+        for(let e = maxtimerAmount; e > -1; e--){
+            if(!isTimerAllowed){
+                break; 
+            }
+            timerAmount--
+            document.getElementById("Timer").style.width = 100/maxtimerAmount*timerAmount + "%";
+            console.log(timerAmount)
+            if(!isTimerAllowed){
+                break; 
+            }
+            if(timerAmount <= 0){
                 missedResults++;
                 document.getElementById("missedResults").innerText = missedResults;
                 isAllowed = false;
@@ -169,10 +180,8 @@ async function startTimer(){
                 isTimerAllowed = false;
                 restartGame()
             }
-            await delay(2); 
-                
-    } 
-    
-    
+            await delay(guessTime);           
+        }   
+    }
 }
 
